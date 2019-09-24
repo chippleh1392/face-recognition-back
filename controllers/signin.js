7cce5a90ebbd6593f2425e21553c27b1cb8f7a5d
chippleh1392 <chippleh@neiu.edu>
2019-09-24 00:26:40 -0500

    fix

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,27 +1,31 @@
-const handleRegister = (db, bcrypt) => (req, res) => {
-  const { email, name, password } = req.body;
-
-  if (!email || !name || !password) {
+const handleSignIn = (db, bcrypt) => (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
     return res.status(400).json("incorrect data");
   }
 
-  const hash = bcrypt.hashSync(password);
-
-  db.transaction(trx => {
-    trx("users")
-      .insert({ name: name, email: email, joined: new Date() })
-      .returning("*")
-      .then(user => {
-        res.json(user[0]);
-        return trx("login")
-          .insert({ hash: hash, user_id: user[0].id })
-          .returning("user_id");
-      })
-      .then(trx.commit)
-      .catch(trx.rollback);
-  }).catch(err => res.status(400).json("unable to register"));
+  db.select("user_id", "hash")
+    .from("login")
+    .leftJoin("users", "login.user_id", "users.id")
+    .where("users.email", "=", email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(password, data[0].hash);
+      if (isValid) {
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", email)
+          .then(user => {
+            res.json(user[0]);
+          })
+          .catch(err => res.status(400).json("error logging in"));
+      } else {
+        res.status(400).json("wrong credentions");
+      }
+    })
+    .catch(err => res.status(400).json("wrong credentions"));
 };
 
 module.exports = {
-  handleRegister: handleRegister
+  handleSignIn: handleSignIn
 };
--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-require("dotenv").config();
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt-nodejs");
@@ -10,7 +8,6 @@ const formData = require("express-form-data");
 const register = require("./controllers/register.js");
 const signin = require("./controllers/signin.js");
 const profile = require("./controllers/profile.js");
-const image = require("./controllers/image.js");
 
 const app = express();
 app.use(bodyParser.json());
@@ -35,14 +32,10 @@ app.post("/register", register.handleRegister(db, bcrypt));
 
 app.get("/profile/:id", profile.handleProfile(db));
 
-app.put("/image", image.handleImage(db));
-app.post("/imageurl", image.handleApiCall());
-app.post("/image-upload", image.handleImageUpload());
-
 app.get("*", (req, res) => {
   res.send("sorry, nothing here((");
 });
 
-app.listen(process.env.PORT || 3001, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("server starts");
 });
