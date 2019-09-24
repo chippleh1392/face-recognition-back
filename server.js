8b003db6e2bef8f21c169020c77e3086ae40ab3d
chippleh1392 <chippleh@neiu.edu>
2019-09-23 23:46:59 -0500

    updating database

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const knex = require("knex");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-polished-39678",
     user: "postgres",
     password: "Facelift911",
     database: "smart-brain"
